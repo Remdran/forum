2f5c6fb6737b8f6b536a58b7c7ce3a7173fa1e0b
Remdran <kelsyerg@gmail.com>
2017-09-26 13:23:09 +0100

    Setting up a Reply test

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -8,22 +8,36 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class ThreadsTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setUp();
+        
+        $this->thread = factory('App\Thread')->create();
+    }
     
     /** @test */
     public function a_user_can_view_all_threads()
     {
-        $thread = factory('App\Thread')->create();
-        
-        $response = $this->get('/threads');
-        $response->assertSee($thread->title);       
+        $this->get('/threads')->assertSee($this->thread->title);
     }
     
     /** @test */
     public function a_user_can_read_a_single_thread()
     {
-        $thread = factory('App\Thread')->create();
+        $this->get('/threads/' . $this->thread->id)->assertSee($this->thread->title);
+    }
+    
+    /** @test */
+    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        // Given we have a thread (given to us by the setUp() method)
         
-        $response = $this->get('/threads/' . $thread->id);
-        $response->assertSee($thread->title);
+        // And that thread includes replies
+        $reply = factory('App\Reply')->create(['thread_id' => $this->thread->id]);
+        
+        // When we visit a thread page
+        // We should see the replies
+        $this->get('/threads/' . $this->thread->id)->assertSee($reply->body);
     }
 }
