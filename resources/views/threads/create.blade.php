02f512628d098a1b4bda0883ab201080ae88bb67
Remdran <kelsyerg@gmail.com>
2017-10-11 12:19:36 +0100

    Working on filtering by username

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -9,18 +9,36 @@
                     <div class="panel-body">
                         <form action="/threads" method="post">
                             {{ csrf_field() }}
+
+                            <div class="form-group">
+                                <label for="channel_id">Choose a Channel: </label>
+                                <select name="channel_id" id="channel_id" class="form-control" required>
+                                    <option value="">Choose One...</option>
+                                    @foreach ($channels as $channel)
+                                        <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>{{ $channel->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
                             <div class="form-group">
                                 <label for="title">Title:</label>
-                                <input type="text" class="form-control" name="title" id="title">
+                                <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}" required>
                             </div>
 
                             <div class="form-group">
                                 <label for="body">Body:</label>
-                                <textarea type="text" class="form-control" name="body" id="body" rows="8"></textarea>
+                                <textarea type="text" class="form-control" name="body" id="body" rows="8" required>{{ old('body') }}</textarea>
                             </div>
 
                             <button type="submit" class="btn btn-primary">Submit</button>
                         </form> 
+                        @if (count($errors))
+                            <ul class="alert alert-danger">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        @endif
                     </div>
                 </div>
             </div>
