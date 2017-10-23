c4e0a33adba12bd1becc0718664182f0e86cc26e
Remdran <kelsyerg@gmail.com>
2017-10-23 11:15:05 +0100

    Allow a user to filter by popular threads

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -6,9 +6,11 @@ use App\User;
 
 class ThreadFilters extends Filters {
 
-    protected $filters = ['by'];
+    protected $filters = ['by', 'popular'];
 
     /**
+     * Filter the query by a given username
+     *
      * @return mixed
      */
     protected function by($username)
@@ -16,5 +18,17 @@ class ThreadFilters extends Filters {
         $user = User::where('name', $username)->firstOrFail();
 
         return $this->builder->where('user_id', $user->id);
+    }
+
+    /**
+     * Filter the query according to most popular threads
+     *
+     * @return mixed
+     */
+    public function popular()
+    {
+        $this->builder->getQuery()->orders = [];
+
+        return $this->builder->orderBy('replies_count', 'desc');
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,21 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        body {
+            padding-bottom: 100px;
+        }
+
+        .level {
+            display: flex;
+            align-items: center;
+        }
+
+        .flex {
+            flex: 1;
+        }
+    </style>
 </head>
-<body style="padding-bottom: 100px;">
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
@@ -44,9 +57,12 @@
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">                                
                                 <li><a href="/threads">All Threads</a></li>
+
                                 @if (auth()->check())
                                     <li><a href="/threads?by={{ auth()->user()->name }}">My Threads</a></li>     
-                                @endif                           
+                                @endif
+
+                                <li><a href="/threads?popular=1">Popular Threads</a></li>
                             </ul>
                         </li>
                         
